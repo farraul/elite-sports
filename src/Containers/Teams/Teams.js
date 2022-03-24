e3c85c19e28e226e4415793d3bdfecdcdd52fdd6
raul far <raulfarllobell@gmail.com>
2022-03-24 23:54:34 +0100

    edit container teams, create the table and the responsive to the table

--- src/Containers/Teams/Teams.js
+++ src/Containers/Teams/Teams.js
@@ -1,19 +1,46 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import './Teams.css';
 
 const Teams = () => {
 
-    useEffect( () => {
+    const [teams, setTeams] = useState([]);
+
+    useEffect(() => {
         takeData();
-    }, [] );
+    }, []);
 
     const takeData = async () => {
-            let res = await axios.get( "http://api.football-api.com/2.0/competitions?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76" );
-            console.table(res.data)
+        let res = await axios.get("http://api.football-api.com/2.0/competitions?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76");
+        setTeams(res.data);
+        console.table(teams);
     }
 
     return (
-        <div>Teams</div>
+        <div>
+            {teams[0] &&
+
+                <div className='teams-container-table'>
+
+
+
+                    {teams.map((league) =>
+                        <div key={league.id} className='teams-container-table-unique'>
+                            <div className='teams-container-table-unique-email'>
+                                {league.id}
+                            </div>
+                            <div className='teams-container-table-unique-name'>
+                                {league.name}
+                            </div>
+                            <div className='teams-container-table-unique-region'>
+                                {league.region}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            }
+
+        </div>
     )
 };
 
